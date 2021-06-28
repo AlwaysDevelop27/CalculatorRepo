18a81a3db9b137792ca14f6cdc16bc2fbd869d5b
learningautomation <learningautomation27@gmail.com>
2021-06-28 11:57:45 +0530

    my clculator master branch code

---
+++ Calculator/src/main/java/com/java/calculator/Addition.java
@@ -0,0 +1,9 @@
+package com.java.calculator;
+
+public class Addition {
+
+	public void add(int a,int b) {
+		int c = a+b;
+		System.out.println("addition value is: "+c);
+	}
+}
---
+++ Calculator/src/main/java/com/java/calculator/Calculator.java
@@ -0,0 +1,16 @@
+package com.java.calculator;
+
+public class Calculator extends Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=6;
+		Calculator c = new Calculator();
+		Addition ax = new Addition();
+		ax.add(a, b);
+		
+
+	}
+
+}
